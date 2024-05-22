5bf463b8d0176fd21fa607466dc0aa10b41a6830
mattley <matvejperepelkin@gmail.com>
2024-05-22 17:43:37 +0600

    Add Custom vector

---
+++ Gameserver/Gameserver/Movement/Vector.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Gameserver.Movement
+{
+    public class Vector
+    {
+        public int dim => _coords.Length;
+        private readonly int[] _coords;
+
+        public Vector(int[] coords)
+        {
+            if (coords.Length == 0)
+            {
+                throw new ArgumentException("Enormous vector input");
+            }
+
+            _coords = coords;
+        }
+        public static Vector operator +(Vector vec1, Vector vec2)
+        {
+
+            if (vec1.dim != vec2.dim)
+            {
+                throw new ArgumentException("Dimensions doesn't match");
+            }
+            return new Vector(vec1._coords.Zip(vec2._coords, (a, b) => a + b).ToArray());
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj != null && _coords.SequenceEqual(((Vector)obj)._coords);
+        }
+        public override int GetHashCode() => _coords.GetHashCode();
+    }
+}
