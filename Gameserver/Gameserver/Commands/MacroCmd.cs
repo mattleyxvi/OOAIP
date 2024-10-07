e9129c85ed6de15ad0dc249b007c5400bc5faa7f
mattley <matvejperepelkin@gmail.com>
2024-10-07 15:51:09 +0600

    Fix whitespaces

--- Gameserver/Gameserver/Commands/MacroCmd.cs
+++ Gameserver/Gameserver/Commands/MacroCmd.cs
@@ -5,7 +5,7 @@ namespace Gameserver.Commands
     public class MacroCmd : ICommand
     {
         private readonly IEnumerable<ICommand> _commands;
-        public MacroCmd(IEnumerable<ICommand> commands) 
+        public MacroCmd(IEnumerable<ICommand> commands)
         {
             _commands = commands;
         }
