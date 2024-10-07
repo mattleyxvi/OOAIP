9b09f72fd2eca3c3e4f07b45d3ab8751f499308e
mattley <matvejperepelkin@gmail.com>
2024-10-07 15:48:25 +0600

    Add MacroCmd and interfaces

---
+++ Gameserver/Gameserver/Commands/InitMacroCmd.cs
@@ -0,0 +1,16 @@
+﻿using Gameserver.Interfaces;
+using Hwdtech;
+
+namespace Gameserver.Commands
+{
+    public class InitMacroCmd : IStrategy
+    {
+        public object Strategy(params object[] args)
+        {
+            (var cmdname, var uobject) = ((string)args[0], (IUObject)args[1]);
+            var commands = IoC.Resolve<IEnumerable<string>>("Gameserver.Config." + cmdname)
+            .Select(command => IoC.Resolve<Interfaces.ICommand>(command, uobject)).ToList();
+            return IoC.Resolve<Interfaces.ICommand>("Gameserver.Command.Macrocommand", commands);
+        }
+    }
+}
