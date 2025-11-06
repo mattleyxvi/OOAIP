6c76899ee610c34a65d8cd09f2dd95bdb7aff481
mattley <matvejperepelkin@gmail.com>
2025-11-06 15:56:52 +0600

    feat: RegisterCommandsCmd

---
+++ Gameserver/Gameserver/Commands/RegisterCommandsCmd.cs
@@ -0,0 +1,18 @@
+﻿using Gameserver.Interfaces;
+using Hwdtech;
+
+namespace Gameserver.Commands
+{
+    public class RegisterCommandsCmd : Interfaces.ICommand
+    {
+        public void Execute()
+        {
+            var dependencies = IoC.Resolve<IDictionary<string, IStrategy>>("Gameserver.Dependencies.Get");
+
+            foreach (var dep in dependencies)
+            {
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Gameserver.Command" + dep.Key, (object[] args) => dep.Value.Strategy(args)).Execute();
+            }
+        }
+    }
+}
