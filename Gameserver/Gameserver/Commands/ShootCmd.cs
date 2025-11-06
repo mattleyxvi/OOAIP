e4d08c0ecb198ac6bde4dc1ad730a324c91bf179
mattley <matvejperepelkin@gmail.com>
2025-11-06 15:58:21 +0600

    feat: Shootcmd and IBullet

---
+++ Gameserver/Gameserver/Commands/ShootCmd.cs
@@ -0,0 +1,21 @@
+﻿using Gameserver.Interfaces;
+using Hwdtech;
+
+namespace Gameserver.Commands
+{
+    public class ShootCmd : Interfaces.ICommand
+    {
+        private readonly IBullet _bullet;
+        public ShootCmd(IBullet bullet)
+        {
+            _bullet = bullet;
+        }
+
+        public void Execute()
+        {
+            var bullet = IoC.Resolve<object>("Gameserver.Create.Bullet", _bullet);
+            var cmd = IoC.Resolve<Interfaces.ICommand>("Gameserver.Create.Bullet.Move", bullet);
+            IoC.Resolve<Interfaces.ICommand>("Gameserver.Queue.Add", cmd).Execute();
+        }
+    }
+}
---
+++ Gameserver/Gameserver/Interfaces/IBullet.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Gameserver.Interfaces
+{
+    public interface IBullet : IMovement
+    {
+        public string BulletType { get; set; }
+    }
+}
