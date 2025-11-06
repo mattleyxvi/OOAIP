2d4c2d980ad4807887da8d94f35812a0f7ceee00
mattley <matvejperepelkin@gmail.com>
2025-11-06 15:58:55 +0600

    feat: Create commands strategies

---
+++ Gameserver/Gameserver/Strategies/CreateStartMoveCmdStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Gameserver.Commands;
+using Gameserver.Interfaces;
+using Hwdtech;
+
+namespace Gameserver.Strategies
+{
+    public class CreateStartMoveCmdStrategy : IStrategy
+    {
+        public object Strategy(params object[] args)
+        {
+            var obj = args[0];
+            var cmd = new StartMovementCmd(IoC.Resolve<IStartable>("Gameserver.Adapter.Create", obj, typeof(IStartable)));
+            return cmd;
+        }
+    }
+}
